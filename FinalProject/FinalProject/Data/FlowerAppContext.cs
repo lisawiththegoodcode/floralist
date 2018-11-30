e4d3aa837eaf9e7edaab66a224e6510853cd3df0
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-11-30 12:02:29 -0600

    added the db context, connection string, configure services, and configuration

---
+++ FinalProject/FinalProject/Data/FlowerAppContext.cs
@@ -0,0 +1,32 @@
+﻿using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data
+{
+    public class FlowerAppContext : DbContext
+    {
+        public FlowerAppContext(DbContextOptions<FlowerAppContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //need help thinking this through
+            modelBuilder.Entity<Proposal>().HasKey(x => x.Id).ForSqlServerIsClustered();
+            modelBuilder.Entity<Proposal>().Property(x => x.Id).UseSqlServerIdentityColumn();
+        }
+        //do we need a dbset for each model?
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Designer> Designers { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Proposal> Proposals { get; set; }
+        public DbSet<ProposalItem> ProposalItems { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
--- FinalProject/FinalProject/Startup.cs
+++ FinalProject/FinalProject/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,7 +33,9 @@ namespace FinalProject
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<FlowerAppContext>(config => config.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
 
+        
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -58,6 +62,18 @@ namespace FinalProject
                     name: "default",
                     template: "{controller=Welcome}/{action=Index}/{id?}");
             });
+            EnsureDatabaseUpdated(app);
+        }
+
+
+        private void EnsureDatabaseUpdated(IApplicationBuilder app)
+        {
+            var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+            using (var serviceScope = scopeFactory.CreateScope())
+            using (var context = serviceScope.ServiceProvider.GetService<FlowerAppContext>())
+            {
+                context.Database.Migrate();
+            }
         }
     }
 }
