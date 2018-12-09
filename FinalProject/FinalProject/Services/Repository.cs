c1836e8f4a229d333cb5d676ed1025e497c79b31
lisa umhey <lisa.umhey@gmail.com>
2018-12-09 15:53:11 -0600

    started repository

---
+++ FinalProject/FinalProject/Services/Repository.cs
@@ -0,0 +1,18 @@
+﻿using FinalProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Services
+{
+    public class Repository
+    {
+        private readonly FlowerAppContext _flowerAppContext;
+
+        public Repository(FlowerAppContext flowerAppContext)
+        {
+            _flowerAppContext = flowerAppContext;
+        }
+    }
+}
