16e52def0670a4417f314328b12167a2c5b9d831
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-12-19 15:28:26 -0600

    added a migration after adding FileImage property. After an exception, I commented this migration out

--- FinalProject/FinalProject/Migrations/20181219211539_AddedBytePropertyToImageModel.Designer.cs
+++ FinalProject/FinalProject/Migrations/20181219211539_AddedBytePropertyToImageModel.Designer.cs
@@ -4,18 +4,20 @@ using FinalProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinalProject.Migrations
 {
     [DbContext(typeof(FlowerAppContext))]
-    partial class FlowerAppContextModelSnapshot : ModelSnapshot
+    [Migration("20181219211539_AddedBytePropertyToImageModel")]
+    partial class AddedBytePropertyToImageModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVimersion", "2.1.4-rtm-31024")
+                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
