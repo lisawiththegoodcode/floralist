3f4f140641fc07de47d0389189e95c29117408d3
lisa umhey <lisa.umhey@gmail.com>
2018-12-30 15:58:21 -0600

    post sunday 12.30 meeting - integrated email sender

---
+++ FinalProject/FinalProject/Services/EmailSender.cs
@@ -0,0 +1,32 @@
+﻿using FluentEmail.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Services
+{
+    public class EmailSender
+    {
+        private IFluentEmail _email;
+
+        public EmailSender([FromServices]IFluentEmail email)
+        {
+            _email = email;
+        }
+
+        public void sendProposalViewEmail()
+        {
+            var email = _email
+                //.From("floralisttheapp@gmail.com")
+                .To("nashid77@yahoo.com")
+                .Subject("hi from floralist")
+                .Body("hi again");
+            //.UsingTemplateFromFile($"{Directory.GetCurrentDirectory()}/Mytemplate.cshtml", new { Name = "Rad Dude" });
+
+            email.Send();
+        }
+    }
+}
--- FinalProject/FinalProject/Startup.cs
+++ FinalProject/FinalProject/Startup.cs
@@ -41,7 +41,21 @@ namespace FinalProject
 
             services.AddDbContext<FlowerAppContext>(config => config.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
             //services.AddDbContext<FlowerAppContext>(config => config.UseInMemoryDatabase("FlowerDb"));        
+
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine(Environment.GetEnvironmentVariable("SENDGRID_API"));
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("***********************************************");
+            services
+                    .AddFluentEmail("floralisttheapp@gmail.com")
+                    .AddRazorRenderer()
+                    .AddSendGridSender(Environment.GetEnvironmentVariable("SENDGRID_API"));
+            services.AddScoped<EmailSender>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
