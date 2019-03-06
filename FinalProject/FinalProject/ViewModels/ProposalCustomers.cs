e98d907f5342d582b580ea896b292400d4933d4f
Lisa Umhey <38416227+lisawiththegoodcode@users.noreply.github.com>
2019-03-06 14:15:14 -0600

    Revert "Revert "fixed customers bug""

---
+++ FinalProject/FinalProject/ViewModels/ProposalCustomers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Models;
+
+namespace FinalProject.ViewModels
+{
+    public class ProposalCustomers
+    {
+        public Proposal Proposal { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}
